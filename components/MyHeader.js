16004f1fc1d5a4e959619f6d4227fb5ae64362c5
ad1012 <69478216+ad1012@users.noreply.github.com>
2021-06-26 16:17:39 -0500

    Add files via upload

--- components/MyHeader.js
+++ components/MyHeader.js
@@ -1,14 +1,15 @@
-import React,{Component} from 'react'
-import {Header,Icon,Badge} from 'react-native-elements'
-import {View,Text,Alert,StyleSheet, ColorPropType} from 'react-native'
+import React, { Component} from 'react';
+import { Header,Icon,Badge } from 'react-native-elements';
+import { View, Text, StyeSheet} from 'react-native';
+import db from '../config'
+import firebase from 'firebase';
 
-const MyHeader = props =>{
-    return(
-        <Header
-        centerComponent={{
-            text:props.title,style:{color:'white',fontSize:30,fontWeight:"bold",}
-        }}
-        backgroundColor="tan"/>
-    )
-}
-export default MyHeader
+const MyHeader = props => { 
+  return ( 
+  <Header centerComponent={{ text: props.title, style: { color: '#90A5A9', fontSize:20,fontWeight:"bold", } 
+}} 
+backgroundColor = "#eaf8fe" /> 
+  ); 
+}; 
+
+export default MyHeader;
